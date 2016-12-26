18b1e8cafed59834cf6008976c7b527ed325ffac
Ron Lee <857748998@qq.com>
2016-12-26 15:07:16 +0800

    Add files via upload

---
+++ database.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>数据服务</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+		<meta name="Keywords" content="湖北省武汉市,华中农业大学,信息学院,张红雨教授,生物信息,农业生物信息湖北省重点实验室"/>
+		<meta name="author" content="李姜,Ron Lee,sdj"/>
+		<link rel="shortcut icon" href="favicon.ico" />
+		<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.css"/>
+		<script src="js/jquery-2.0.0.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+		<link rel="stylesheet" type="text/css" href="css/index.css"/>
+		<link rel="stylesheet" type="text/css" href="css/foot.css"/>
+		<style type="text/css">
+			.col-md-4:hover{
+				box-shadow: 0 0 24px;
+			}
+			.col-md-3:hover{
+				box-shadow: 0 0 24px;
+			}
+		</style>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("nav ul li:eq(3)").addClass("active");
+			});
+		</script>
+	</head>
+	<body>
+		<?php
+			include('./Page/header.php');
+			?>
+		<div class="container-fluid" style="background-color:#F0F5F9;margin-top:2%;">
+			<div class="caption" style="margin-top: 2%;">
+				<h1 align="center">数据服务</h1>
+				<h3 align="center" style="color: #828282;font-family:sans-serif;">本网站提供其他实验室的数据库服务和生物信息网站链接,如有任何问题，请与相关网站负责人联系</h3>
+			</div>
+			<div class="container" style="margin-top: 4.5%;">
+				<div class="row">
+					<div class="col-md-4" style="background-color: white;width: 32%;margin-right: 2%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">Amphioxus</h3>
+						</div>
+						<p class="text-center" style="color:gray">文昌鱼基因组功能注释数据库</p>
+						<p class="text-center"><a href="http://ibi.hzau.edu.cn/amphioxus/" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+					<div class="col-md-4" style="background-color: white;width: 32%;margin-right: 2%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">OGAJ</h3>
+						</div>
+						<p class="text-center" style="color:gray">柑橘基因组数据库</p>
+						<p class="text-center"><a href="http://citrus.hzau.edu.cn/orange/" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+					<div class="col-md-4" style="background-color: white;width: 32%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">PlantNATsDB</h3>
+						</div>
+						<p class="text-center" style="color:gray">植物自然反义转录数据库</p>
+						<p class="text-center"><a href="http://bis.zju.edu.cn/pnatdb/" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="container" style="margin-top: 4%;margin-bottom: 4%;">
+				<div class="row">
+					<div class="col-md-3" style="background-color: lightskyblue;width: 24%;margin-right: 1%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">BioRel</h3>
+						</div>
+						<p class="text-center" style="color:gray">生物相关性网络服务</p>
+						<p class="text-center"><a href="http://ibi.hzau.edu.cn/biorel" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+					<div class="col-md-3" style="background-color: lightskyblue;width: 24%;margin-right: 1%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">RBRDetector</h3>
+						</div>
+						<p class="text-center" style="color:gray">RNA结合位点预测</p>
+						<p class="text-center"><a href="http://ibi.hzau.edu.cn/rbrdetector" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+					<div class="col-md-3" style="background-color: lightskyblue;width: 24%;margin-right: 1%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">AutoChrom3D</h3>
+						</div>
+						<p class="text-center" style="color:gray">3D染色质结构建模</p>
+						<p class="text-center"><a href="http://ibi.hzau.edu.cn/3dmodel" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+					<div class="col-md-3" style="background-color: lightskyblue;width: 25%;">
+						<div class="caption">
+							<h3 align="center" style="color: #FA3859;">FDserver</h3>
+						</div>
+						<p class="text-center" style="color:gray">Protein Folding Server</p>
+						<p class="text-center"><a href="http://ibi.hzau.edu.cn/FDserver" target="_blank" class="btn btn-success btn-lg" role="button">GO TO</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<?php
+			include('./Page/foot.php');
+			?>
+	</body>
+</html>
---
+++ intro.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>实验室简介</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+		<meta name="Keywords" content="湖北省武汉市,华中农业大学,信息学院,张红雨教授,生物信息,农业生物信息湖北省重点实验室"/>
+		<link rel="shortcut icon" href="favicon.ico" />
+		<meta name="author" content="李姜,Ron Lee,sdj"/>
+		<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.css"/>
+		<script src="js/jquery-2.0.0.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+		<link rel="stylesheet" type="text/css" href="css/index.css"/>
+		<link rel="stylesheet" type="text/css" href="css/foot.css"/>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("nav ul li:eq(1)").addClass("active");
+			});
+		</script>
+	</head>
+	<body>
+		<?php
+			include('./Page/header.php');
+			?>
+		<div class="container-fluid" style="background-color:#F0F5F9;margin-top:2%;">
+			<div class="caption" style="margin-top: 2%;">
+				<h2 align="center" style="font-weight: 600">实验室概况</h2>
+			</div>
+			<div class="container" style="margin-top: 1.5%;">
+				<div class="row">
+					<div class="col-md-5">
+						<img src="img/hzau01.jpg" class="img-rounded"/>
+					</div>
+					<div class="col-md-7">
+					<p style="line-height:33pt;font-size: 1.2em;">&nbsp;&nbsp; 华中农业大学农业生物信息湖北省重点实验室的定位是以应用基础研究为主、并向基础与应用研究延伸。
+						本实验室以湖北省主要作物、家畜及相关致病微生物的基因组、蛋白质组、代谢组为研究对象，瞄准学科前沿将各种"组学"技术与生物信息学技术相结合，分别从计算基因组学、计算蛋白质组学、计算代谢组学与药物信息学方面开展全方位的研究工作，深入解读各种"组学"产生的海量生物学数据，
+						为动植物育种、种质资源保护、农业病虫害监测和防治提供新理论、新技术和新方法，为确保我省乃至周边省份的粮食安全做出贡献。
+					</p>
+					</div>
+				</div>
+			</div>
+			<div class="caption" style="margin-top: 2%;">
+				<h2 align="center" style="font-weight: 600">实验室详情</h2>
+			</div>
+			<div class="container" style="margin-top: 3%;margin-bottom: 2%;">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="panel panel-success">
+					    <div class="panel-heading">
+					        <h3 class="panel-title text-center">我们的成立</h3>
+					    </div>
+					    <div class="panel-body" style="font-size: 1.2em;">
+					        <p>&nbsp;&nbsp;2011年5月，华中农业大学为了促进生物信息学科的发展，成立了生物信息中心。
+					        	经过2年多的建设，该中心初具规模，拥有了比较齐全的生物信息学研究方面的软硬件设备，并在作物基因组学和动物药物发现方面取得了比较突出的成绩。
+					        	在该中心基础上，华中农业大学于2013年申报农业生物信息湖北省重点实验室，并于当年12月正式获得批准
+					        </p>
+					    </div>
+					</div>
+					</div>
+					<div class="col-md-4">
+						<div class="panel panel-info">
+						    <div class="panel-heading">
+						        <h3 class="panel-title text-center">我们的团队</h3>
+						    </div>
+						    <div class="panel-body" style="font-size: 1.2em;">
+						        <p>&nbsp;&nbsp;目前实验室已拥有一支以中青年为主体、学术思想活跃、学风优良、工作勤奋、勇于创新、结构合理的学术队伍。实验室现有固定研究人员18人（16人拥有博士学位），
+						        	其中教授4人（含青年千人1人、楚天学者特聘教授2人、
+						        	湖北省高端人才引领培养计划1人、教育部新世纪人才1人），平均年龄34.2岁，核心团队入选湖北省自然科学基金创新群体</p>
+						    </div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="panel panel-danger">
+					    <div class="panel-heading">
+					        <h3 class="panel-title text-center">我们的目标</h3>
+					    </div>
+					    <div class="panel-body" style="font-size: 1.2em;">
+					        <p>&nbsp;&nbsp;本实验室将以我省主要作物、畜禽及相关致病微生物的基因组、转录组、蛋白质组、代谢组和表型组为研究对象，瞄准学科前沿，
+					        	分别从基因组信息挖掘、蛋白质序列结构功能分析、代谢组分析与药物设计、表型组分析和生物信息技术研发等五个方面开展全方位的研究，
+					        	深入解读各种“组学”产生的海量生物学数据，为确保我省乃至周边省份的粮食安全做出贡献
+					        	</p>
+					    </div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+		<?php
+			include('./Page/foot.php');
+			?>
+	</body>
+</html>
