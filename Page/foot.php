ce4d000ed8188886e6840df161a4cdd1c61cee41
Ronlee12355 <857748998@qq.com>
2016-12-26 15:31:02 +0800

    addition files

---
+++ Page/foot.php
@@ -0,0 +1,48 @@
+<div class="container-fluid" id="footerContainer" >
+			<div class="container" style="margin-top: 10px;padding-top: 5px;">
+			<div class="row" style="padding-top: 5px;">
+			  <div class="col-md-2">
+			  	<h3><span class="glyphicon glyphicon-th-list"></span><strong>数据服务</strong></h3>
+				<ul class="list-unstyled">
+					<li><a href="./database.php">数据库</a></li>
+					<li><a href="./database.php">网络服务</a></li>
+				</ul>
+			  </div>
+			  <div class="col-md-2">
+			  	<h3><span class="glyphicon glyphicon-th-list"></span><strong>研究方向</strong></h3>
+				<ul class="list-unstyled">
+					<li><a href="./show_project.php?num=0">计算基因组学</a></li>
+					<li><a href="./show_project.php?num=1">三维染色质结构建模</a></li>
+					<li><a href="./show_project.php?num=2">代谢组学与药物信息学</a></li>
+					<li><a href="./show_project.php?num=3">系统生物学与计算蛋白质组学</a></li>
+				</ul>
+			  </div>
+			  <div class="col-md-2">
+			  	<h3><span class="glyphicon glyphicon-th-list"></span><strong>其他内容</strong></h3>
+				<ul class="list-unstyled">
+					<li><a href="./contact.php">联系我们</a></li>
+					<li><a href="./project.php">课题</a></li>
+					<li><a href="./member.php">实验室成员</a></li>
+				</ul>
+			  </div>
+			  <div class="col-md-6" style="padding-left: 200px;padding-top: 2px;">
+			  	<h3><strong><p>感谢您浏览我们的网站</p></strong></h3>
+		  		<address>
+					  <strong>HZAU 华中农业大学</strong><br>
+					  狮子山街一号 430070<br>
+					  湖北省武汉市洪山区<br>
+					  <abbr title="Phone">P:</abbr> +86-27-8728-0877
+					</address>
+		      <strong><p>如果您对我们的网站有什么建议或者是您对我们的研究领域感兴趣, 如果您愿意与我们合作或者交流</p></strong>
+		      <address>
+				  <strong>欢迎联系我们</strong><span class="glyphicon glyphicon-envelope" style="margin-left: 2px;padding-top: 2px;"></span><br>
+				  <a href="mailto:feedback@ibi.hzau.edu.cn">feedback@ibi.hzau.edu.cn</a>
+				</address>
+			</div>
+			</div>
+			<hr style="color: lightgrey;" />
+			</div>
+			<div class="container">
+				<p id="foot_p">Designed by <b>李&nbsp;姜</b> Ron Lee. If you have any question about the website, please send an email to Ronlee12355@outlook.com<span class="glyphicon glyphicon-envelope"></span>||2016年</p>
+			</div>
+		</div>
